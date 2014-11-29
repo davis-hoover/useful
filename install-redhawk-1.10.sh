4a28eba14ec81c34a95c306215cea3e12093e350
dhoover-geon <dhoover@geontech.com>
2014-11-29 12:54:27 -0500

    temp save (yet to be tested), directory stdout & stderr to log files, made install happen in a newly created directory of format SOMEUUID-temp-redhawk-install, cleanup still happens for rpms and tarballs, but UUID directory and logs remain

--- install-redhawk-1.10.sh
+++ install-redhawk-1.10.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 # install REDHAWK 1.10 on centOS 6
-wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-sudo yum install -y --nogpgcheck epel-release-*.noarch.rpm
-sudo yum install -y omniORB-servers log4cxx-devel omniORB-devel xsd
-sudo yum install -y libuuid-devel boost-devel autoconf automake libtool expat-devel java-devel python-devel numpy PyQt4
-wget http://sourceforge.net/projects/redhawksdr/files/redhawk/1.10.0/el6/x86_64/redhawk-yum-1.10.0-10-el6-x86_64.tar.gz
-tar xzf redhawk-yum-*.tar.gz
-cd redhawk-1.10*
-sudo yum install -y --nogpgcheck *.rpm
-. /etc/profile.d/redhawk.sh
-. /etc/profile.d/redhawk-sdrroot.sh
-sudo /usr/sbin/usermod -a -G redhawk $USER
-echo "InitRef = EventService=corbaloc::127.0.0.1:11169/omniEvents" >> /etc/omniORB.cfg
-sudo $OSSIEHOME/bin/cleanomni
-sudo /sbin/chkconfig --level 345 omniNames on
-sudo /sbin/chkconfig --level 345 omniEvents on
-rm epel-release-*.noarch.rpm
-rm redhawk-yum-*.tar.gz
-rm redhawk-1.10*
+
+INITIAL_WD=$pwd
+INSTALL_DIR="$(uuidgen)-temp-redhawk-install
+
+mkdir $INSTALLDIR
+cd $INSTALLDIR
+
+echo "1/3    Downloading..."
+wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/downloading.log
+wget http://sourceforge.net/projects/redhawksdr/files/redhawk/1.10.0/el6/x86_64/redhawk-yum-1.10.0-10-el6-x86_64.tar.gz < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/downloading.log
+
+echo "2/3    Installing...."
+sudo yum install -y --nogpgcheck epel-release-*.noarch.rpm < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+sudo yum install -y omniORB-servers log4cxx-devel omniORB-devel xsd < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+sudo yum install -y libuuid-devel boost-devel autoconf automake libtool expat-devel java-devel python-devel numpy PyQt4 < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+
+
+tar xzf redhawk-yum-*.tar.gz < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+cd redhawk-1.10* < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+sudo yum install -y --nogpgcheck *.rpm < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+. /etc/profile.d/redhawk.sh < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+. /etc/profile.d/redhawk-sdrroot.sh < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+sudo /usr/sbin/usermod -a -G redhawk $USER < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+echo "InitRef = EventService=corbaloc::127.0.0.1:11169/omniEvents" >> /etc/omniORB.cfg < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+sudo $OSSIEHOME/bin/cleanomni < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+sudo /sbin/chkconfig --level 345 omniNames on < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+sudo /sbin/chkconfig --level 345 omniEvents on < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/installing.log
+
+echo "3/3    Performing cleanup...."
+rm epel-release-*.noarch.rpm < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/cleanup.log
+rm redhawk-yum-*.tar.gz < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/cleanup.log
+rm redhawk-1.10* < /dev/null > /dev/null 2>&1 >> $INITIAL_WD/$INSTALL_DIR/cleanup.log
+
+echo "done"
+
 sudo shutdown -hr now
 
