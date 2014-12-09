9ddae3a6133a9a70545c2ea0468d46bb0efa39fd
dhoover <dhoover@geontech.com>
2014-12-09 14:16:32 -0500

    added pidgin

--- ubuntu-default-apps.sh
+++ ubuntu-default-apps.sh
@@ -5,4 +5,5 @@ sudo apt-get install -y vim
 sudo apt-get install -y virtualbox
 sudo apt-get install -y docky
 sudo apt-get install -y compizconfig-settings-manager
+sudo apt-get install -y pidgin
 
