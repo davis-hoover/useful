6d167af2adea4b4c7ec47dcd466e37dfbc6b7198
dhoover <dhoover@geontech.com>
2014-12-09 14:15:05 -0500

    initial commit

---
+++ ubuntu-default-apps.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/evn sh
+sudo add-apt-repository ppa:docky-core/stable
+sudo apt-get update
+sudo apt-get install -y vim
+sudo apt-get install -y virtualbox
+sudo apt-get install -y docky
+sudo apt-get install -y compizconfig-settings-manager
+
