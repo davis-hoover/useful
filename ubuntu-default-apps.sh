058e7e30b9780db1c1ceeed738dff66ac2585275
dhoover <dhoover@geontech.com>
2014-12-09 14:40:20 -0500

    added spotify

--- ubuntu-default-apps.sh
+++ ubuntu-default-apps.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/evn sh
 sudo add-apt-repository ppa:docky-core/stable
+sudo add-apt-repository "deb http://repository-origin.spotify.com stable non-free"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
 sudo apt-get update
+
 sudo apt-get install -y vim
 sudo apt-get install -y virtualbox
 sudo apt-get install -y docky
 sudo apt-get install -y compizconfig-settings-manager
 sudo apt-get install -y pidgin
+sudo apt-get install -y spotify-client
 
