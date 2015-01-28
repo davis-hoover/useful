0234ebc92b7a376275f6e91103f2d0ed4449171e
davis-hoover <davis.hoover@gmail.com>
2015-01-28 16:14:31 -0500

    fixed typo and added wine

--- ubuntu-default-apps.sh
+++ ubuntu-default-apps.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/evn sh
+#!/usr/bin/env sh
 sudo add-apt-repository ppa:docky-core/stable
 sudo add-apt-repository "deb http://repository-origin.spotify.com stable non-free"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
@@ -10,4 +10,5 @@ sudo apt-get install -y docky
 sudo apt-get install -y compizconfig-settings-manager
 sudo apt-get install -y pidgin
 sudo apt-get install -y spotify-client
+sudo apt-get install -y wine winetricks
 
